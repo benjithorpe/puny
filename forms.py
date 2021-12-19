c22aad8f1ee2cf2198ffe0ab4303d3b1ab53377d
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2021-12-19 07:04:46 +0000

    Add macros, update form errors display

--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[
-                           DataRequired(), Length(min=4, max=20),
+    username = StringField("Username", validators=[DataRequired(),
+                           Length(min=4, max=20),
                         ])
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[
-                                     DataRequired(), EqualTo("password"),
+                                     DataRequired(),
+                                     EqualTo("password",
+                                        message="Passwords must be equal."),
                                      ])
     submit = SubmitField("Register")
 
